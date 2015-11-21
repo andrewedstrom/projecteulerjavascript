0bd0d44b4d69346e0984ae2174ad760a74d6f1f3
andrewedstrom <andrewedstrom@gmail.com>
2015-11-21 15:27:31 -0600

    finished 23

--- 23.js
+++ 23.js
@@ -1,5 +1,5 @@
 var finalSum = 0;
-var array = [];
+var abundants = [];
 var tmp = 0;
 
 //find abundant numbers
@@ -12,9 +12,26 @@ for (var i = 1; i < 28124; i++) {
 		}
 	}
 	if (tmp > i) {
-		array.push(i);
+		abundants.push(i);
 	}
 }
 
+
 //find sum of positive integers which cannot be written as 
 //the sum of two abundant numbers
+
+var array = [];
+for (var i = 1; i < 28124; i++) {
+	var found = false;
+	for(var j = 0; abundants[j] < i && j< abundants.length && !found; j++) {
+		for(var k = j; abundants[k] <= i - abundants[j] && k < abundants.length && !found; k++) {
+			if (abundants[j] + abundants[k] === i) {
+				found = true;
+			}
+		}
+	}
+	if (!found) finalSum += i;
+	if (i%1000 === 0) console.log('i', i);
+}
+
+console.log('The sum of all the positive integers which cannot be written as the sum of two abundant numbers is ', finalSum);
